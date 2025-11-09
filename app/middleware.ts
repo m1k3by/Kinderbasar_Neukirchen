ddf11b118b0d22946883ce3a8b86366bff06def2
Stöcker <michael.stoecker@l-p-a.com>
2025-11-09 22:52:40 +0100

    fix _middleware import path for cloudflare build

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { env } from '@/lib/env';
+import { env } from './lib/env';
 
 export async function middleware(request: NextRequest) {
   // Protected routes
