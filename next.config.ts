7bc364b21ecb1db44ed7ca80e5ccfd10bd12f3e3
m1k3by <github@michael-stoecker.com>
2026-04-13 23:04:19 +0200

    fix built errors

--- next.config.ts
+++ next.config.ts
@@ -77,6 +77,8 @@ const nextConfig: NextConfig = {
 export default withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    skipWaiting: true,
+  },
 })(nextConfig);
